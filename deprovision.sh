439b18dfe833991d8d56acf34681301cf4381af7
Matthias Seiler <matthiasseiler@outlook.com>
2022-09-01 14:15:29 +0200

    k3s on GCP VMs

--- deprovision.sh
+++ deprovision.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-printf "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n\t Deleting all ressources in the kubernetes cluster...\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n"
-kubectl delete all --all -n airflow
-kubectl delete all --all -n prometheus-grafana
+ZONE=${ZONE:-europe-west3-b}
 
-printf "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n\t Deleting Persistent Volume Claims...\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n"
-kubectl delete pvc --all -n airflow
+delete_deploymentes() {
+    printf "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n\t Deleting all ressources in the kubernetes cluster...\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n"
+    kubectl delete all --all -n airflow
+    kubectl delete all --all -n prometheus-grafana
 
-printf "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n\t Deleting GKE cluster...\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n"
-gcloud container clusters delete cluster-1 --region=europe-west3-b
+    printf "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n\t Deleting Persistent Volume Claims...\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n"
+    kubectl delete pvc --all -n airflow
+}
 
+delete_vm() {
+    gcloud config set compute/zone $ZONE
+    printf "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n\t Deleting VMs...\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n"
+    gcloud compute instances list \
+        --filter=tags.items=k3s --format="get(name)" |
+        xargs gcloud compute instances delete
+
+    printf "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n\t Deleting Firewall Rule...\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n"
+    gcloud compute firewall-rules delete k3s
+}
+
+########################
+# Main
+########################
+
+delete_deploymentes
+delete_vm
--- setup.sh
+++ setup.sh
@@ -1,48 +1,103 @@
 #!/bin/bash
+
 ###
 # Note: all port forwarding happens in the background. You have to kill the process to stop it.
 ###
 
-printf "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n\t Creating k8s cluster and connecting...\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n"
-gcloud config set project resonant-forge-350508
+set -u
 
-# when conducting final benchmarks, we may want to use better machines
-gcloud container clusters create cluster-1 \
-  --machine-type e2-standard-4 \
-  --image-type "ubuntu_containerd" \
-  --num-nodes 3 \
-  --region "europe-west3-b" \
-  --disk-size 20
+create_vms() {
+  ZONE=${ZONE:-europe-west3-b}
+  MACHINE_TYPE=${MACHINE_TYPE:-e2-medium}
+  IMAGE_PROJECT=${IMAGE_PROJECT:-ubuntu-os-cloud}
+  IMAGE_FAMILY=${IMAGE_FAMILY:-ubuntu-2204-lts}
+  BOOT_DISK_SIZE=${BOOT_DISK_SIZE:-50GB}
+  (
+    set -x
+    gcloud compute instances create k3s-1 \
+      --zone "$ZONE" \
+      --machine-type "$MACHINE_TYPE" \
+      --image-project "$IMAGE_PROJECT" \
+      --image-family "$IMAGE_FAMILY" \
+      --boot-disk-size "$BOOT_DISK_SIZE" \
+      --tags k3s,k3s-master
+
+    gcloud compute instances create k3s-2 k3s-3 \
+      --zone "$ZONE" \
+      --machine-type "$MACHINE_TYPE" \
+      --image-project "$IMAGE_PROJECT" \
+      --image-family "$IMAGE_FAMILY" \
+      --boot-disk-size "$BOOT_DISK_SIZE" \
+      --tags k3s,k3s-worker
+
+    gcloud compute config-ssh
+  )
+}
+
+create_k3s_cluster() {
+  primary_server_ip=$(gcloud compute instances list \
+    --filter=tags.items=k3s-master \
+    --format="get(networkInterfaces[0].accessConfigs.natIP)")
+
+  # Instal k3s with k3sup
+  (
+    set -x
+    k3sup install --ip "${primary_server_ip}" --context k3s --ssh-key ~/.ssh/google_compute_engine --user $(whoami)
+
+    gcloud compute firewall-rules create k3s --allow=tcp:6443 --target-tags=k3s
 
 gcloud container clusters get-credentials cluster-1 --zone europe-west3-b --project resonant-forge-350508
 
-# deploy Airflow
+deploy_airflow() {
+  helm install airflow apache-airflow/airflow \
+    --namespace airflow --create-namespace \
+    -f airflow-values.yaml \
+    --set executor=KubernetesExecutor
+
+  nohup kubectl port-forward service/airflow-webserver 8080:8080 -n airflow &>/dev/null &
+  printf "\nAirflow UI accessible via http://localhost:8080\n  User: pjds\n  Pass: pjds\n"
+}
+
+deploy_ebpf() {
+  helm install ebpf ebpf-exporter-swms \
+    --namespace kube-system \
+    -f ebpf-exporter-swms/values.yaml
+
+  kubectl apply -f ebpf-exporter-swms/clusterrolebinding.yaml
+  
+  printf "\neBPF Exporter installed.\n"
+}
+
+deploy_prometheus_grafana() {
+  helm install prometheus-grafana prometheus-community/kube-prometheus-stack \
+    -n prometheus-grafana --create-namespace \
+    -f prometheus-grafana-values.yaml
+
+  sleep 3s
+  kubectl cp ./dashboards $(kubectl get pod -l app.kubernetes.io/name=grafana -n prometheus-grafana -o jsonpath="{.items[0].metadata.name}"):/tmp -n prometheus-grafana
+  nohup kubectl port-forward service/prometheus-grafana 3000:80 -n prometheus-grafana &>/dev/null &
+  printf "\nGrafana dashboard accessible via http://localhost:3000\n  User: pjds\n  Pass: pjds\n"
+  # nohup kubectl port-forward service/prometheus-grafana-kube-pr-prometheus 9090:9090 -n prometheus-grafana &>/dev/null &
+  # printf "\nPrometheus dashboard accessible via http://localhost:9090\n"
+}
+
+########################
+# Main
+########################
+
+printf "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n\t Creating VMs...\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n"
+create_vms
+sleep 15s # give machines some more time
+printf "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n\t Installing k3s...\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n"
+create_k3s_cluster
+sleep 5s
+export KUBECONFIG=$(pwd)/kubeconfig
+printf "\nKubernetes Nodes:\n"
+kubectl get nodes
+
 printf "\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n\t Deploying Airflow...\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n"
-helm install airflow apache-airflow/airflow \
-  --namespace airflow --create-namespace \
-  -f airflow-values.yaml \
-  --set executor=KubernetesExecutor
-
-nohup kubectl port-forward service/airflow-webserver 8080:8080 -n airflow &>/dev/null &
-printf "\nAirflow UI accessible via http://localhost:8080\n  User: pjds\n  Pass: pjds\n"
-
-# deploy eBPF Exporter
+deploy_airflow
 printf "\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n\t Deploying eBPF Exporter...\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n"
-helm install ebpf ebpf-exporter-swms \
-  --namespace kube-system \
-  -f ebpf-exporter-swms/values.yaml
-
-kubectl apply -f ebpf-exporter-swms/clusterrolebinding.yaml
-
-# deploy Prometheus and Grafana
+deploy_ebpf
 printf "\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n\t Deploying Prometheus and Grafana...\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n"
-helm install prometheus-grafana prometheus-community/kube-prometheus-stack \
-  -n prometheus-grafana --create-namespace \
-  -f prometheus-grafana-values.yaml
-
-nohup kubectl port-forward service/prometheus-grafana 3000:80 -n prometheus-grafana &>/dev/null &
-printf "\nGrafana dashboard accessible via http://localhost:3000\n  User: pjds\n  Pass: pjds\n"
-sleep 3s
-kubectl cp ./dashboards $(kubectl get pod -l app.kubernetes.io/name=grafana -n prometheus-grafana -o jsonpath="{.items[0].metadata.name}"):/tmp -n prometheus-grafana
-# nohup kubectl port-forward service/prometheus-grafana-kube-pr-prometheus 9090:9090 -n prometheus-grafana &>/dev/null &
-# printf "\nPrometheus dashboard accessible via http://localhost:9090\n"
+deploy_prometheus_grafana
